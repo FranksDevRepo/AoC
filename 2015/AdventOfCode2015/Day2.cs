927d87f8cd21782f303b3d21393a279140b9909e
Frank <f.speck@gmx.de>
2019-12-05 02:51:02 +0100

    Advent of Code 2015
    Day 1 - 3

---
+++ 2015/AdventOfCode2015/Day2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode2015
+{
+    public class Day2
+    {
+        [Theory]
+        [InlineData(2, 3, 4, 58)]
+        [InlineData(1, 1, 10, 43)]
+        public void TestWrappingPaper(int length, int width, int height, int expected)
+        {
+            var paper = CalcSurfaceArea(length, width, height);
+            var slack = CalcSlack(length, width, height);
+            Assert.Equal(expected, paper+slack);
+        }
+
+        public static int CalcSurfaceArea(int length, int width, int height)
+        {
+            return 2 * length * width + 2 * width * height + 2 * height * length;
+        }
+
+        public static int CalcSlack(int length, int width, int height)
+        {
+            return Enumerable.Min(new int[] {length * width, width * height, height * length});
+        }
+    }
+}
---
+++ 2015/AdventOfCode2015/Day3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode2015
+{
+    public class Day3
+    {
+        [Theory]
+        [InlineData(">", 2)]
+        [InlineData("^>v<", 4)]
+        [InlineData("^v^v^v^v^v", 2)]
+        public void TestPuzzle(string directions, int expected)
+        {
+
+        }
+    }
+}
