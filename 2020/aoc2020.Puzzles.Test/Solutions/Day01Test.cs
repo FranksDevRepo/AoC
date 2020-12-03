1367c1154321fc282b06a74673a89727183c67df
Frank <frank.speck@txs.de>
2020-12-03 23:02:51 +0100

    setup day 1 dotnet run -p aoc2020.ConsoleApp --setup 1

---
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day01Test.cs
@@ -0,0 +1,23 @@
+﻿using aoc2020.Puzzles.Solutions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2020.Puzzles.Test.Solutions
+{
+    public sealed class Day01Test : TestBase<Day01>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+    }
+}
