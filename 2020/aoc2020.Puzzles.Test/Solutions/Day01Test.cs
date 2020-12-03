6ccf32c311ab842be96699a4f9c5c3b29b63195f
Frank <frank.speck@txs.de>
2020-12-04 00:59:57 +0100

    solved day1 part2

--- 2020/aoc2020.Puzzles.Test/Solutions/Day01Test.cs
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day01Test.cs
@@ -6,23 +6,23 @@ namespace aoc2020.Puzzles.Test.Solutions
 {
     public sealed class Day01Test : TestBase<Day01>
     {
-        [Fact]
-        public async Task Part1()
-        {
-            var input = @"1721
+        private readonly string myInput = @"1721
 979
 366
 299
 675
 1456";
-            Assert.Equal("514579", await Solution.Part1Async(input));
+
+        [Fact]
+        public async Task Part1()
+        {
+            Assert.Equal("514579", await Solution.Part1Async(myInput));
         }
 
         [Fact]
         public async Task Part2()
         {
-            var input = @"";
-            Assert.Equal("", await Solution.Part2Async(input));
+            Assert.Equal("241861950", await Solution.Part2Async(myInput));
         }
     }
 }
--- 2020/aoc2020.Puzzles/Solutions/Day01.cs
+++ 2020/aoc2020.Puzzles/Solutions/Day01.cs
@@ -18,7 +18,23 @@ namespace aoc2020.Puzzles.Solutions
 
         public override async Task<string> Part2Async(string input)
         {
-            throw new NotImplementedException();
+            var expenses = GetExpenses(input);
+            var solution = TripleExists(expenses, 2020);
+            return (solution.Value.x1 * solution.Value.x2 * solution.Value.x3).ToString();
+        }
+
+        private (int x1, int x2, int x3)? TripleExists(IEnumerable<int> numbers, int sum)
+        {
+            var set = new HashSet<int>(numbers);
+            foreach (var number in set)
+            {
+                var pair = PairExists(numbers, sum - number);
+                if (pair.HasValue)
+                {
+                    return (x1: pair.Value.x1, x2: pair.Value.x2, x3: number);
+                }
+            }
+            return null;
         }
 
         private static IEnumerable<int> GetExpenses(string input) => GetLines(input).Select(x => Convert.ToInt32(x));
@@ -26,7 +42,7 @@ namespace aoc2020.Puzzles.Solutions
         static (int x1, int x2)? PairExists(IEnumerable<int> numbers, int sum)
         {
             var set = new HashSet<int>(numbers);
-            foreach (int elem in set)
+            foreach (var elem in set)
                 if (set.Contains(sum - elem))
                 {
                     return (x1: elem, x2: sum - elem);
