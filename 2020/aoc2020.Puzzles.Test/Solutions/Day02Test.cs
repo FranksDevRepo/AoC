2aaf1e6055f2256a2538a0895f5ca1f3047c18cb
Frank <frank.speck@txs.de>
2020-12-06 00:42:30 +0100

    Test day2 part 2

--- 2020/aoc2020.Puzzles.Test/Solutions/Day02Test.cs
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day02Test.cs
@@ -7,20 +7,20 @@ namespace aoc2020.Puzzles.Test.Solutions
 {
     public sealed class Day02Test : TestBase<Day02>
     {
+        private readonly string myInput = @"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc";
+
         [Fact]
         public async Task Part1()
         {
-            var input = @"1-3 a: abcde
-1-3 b: cdefg
-2-9 c: ccccccccc";
-            Assert.Equal("2", await Solution.Part1Async(input));
+            Assert.Equal("2", await Solution.Part1Async(myInput));
         }
 
         [Fact]
         public async Task Part2()
         {
-            var input = @"";
-            Assert.Equal("", await Solution.Part2Async(input));
+            Assert.Equal("1", await Solution.Part2Async(myInput));
         }
 
         [Fact]
