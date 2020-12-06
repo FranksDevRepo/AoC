7bcc800a913b4e54e9e457ef5aac1854f23f20e9
Frank <frank.speck@txs.de>
2020-12-06 04:21:46 +0100

    puzzle day 3 part 2 still something wrong in the tree counting

--- 2020/aoc2020.Puzzles/Solutions/Day03.cs
+++ 2020/aoc2020.Puzzles/Solutions/Day03.cs
@@ -16,6 +16,19 @@ namespace aoc2020.Puzzles.Solutions
             return countTrees.ToString();
         }
 
+        public override async Task<string> Part2Async(string input)
+        {
+            var terrain = input.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(line => line.Select(c => c is '#').ToArray()).ToArray();
+            var countTreesSlope1 = CountTrees(terrain, 1, 1);
+            var countTreesSlope2 = CountTrees(terrain, 3, 1);
+            var countTreesSlope3 = CountTrees(terrain, 5, 1);
+            var countTreesSlope4 = CountTrees(terrain, 7, 1);
+            var countTreesSlope5 = CountTrees(terrain, 1, 2);
+
+            Int64 solution = countTreesSlope1 * countTreesSlope2 * countTreesSlope3 * countTreesSlope4 * countTreesSlope5;
+            return solution.ToString();
+        }
+
         private int CountTrees(bool[][] terrain, int right, int down)
         {
             int countTrees = 0;
@@ -30,11 +43,6 @@ namespace aoc2020.Puzzles.Solutions
             }
 
             return countTrees;
-        }
-
-        public override async Task<string> Part2Async(string input)
-        {
-            throw new NotImplementedException();
         }
     }
 }
