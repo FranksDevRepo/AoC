525b4a19eaf6e7b82d150219a1f8cfd57954ea59
Frank <frank.speck@txs.de>
2020-12-06 04:24:51 +0100

    setup puzzles day 4 + 5

---
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day05Test.cs
@@ -0,0 +1,23 @@
+﻿using aoc2020.Puzzles.Solutions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2020.Puzzles.Test.Solutions
+{
+    public sealed class Day05Test : TestBase<Day05>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+    }
+}
