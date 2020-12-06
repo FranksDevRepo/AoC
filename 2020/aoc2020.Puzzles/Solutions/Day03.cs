3759feef37d6bd544a9ba79b2008753fd324d69d
Frank <frank.speck@txs.de>
2020-12-06 02:57:47 +0100

    solved puzzle day 3 part 1

--- 2020/aoc2020.Puzzles/Solutions/Day03.cs
+++ 2020/aoc2020.Puzzles/Solutions/Day03.cs
@@ -1,7 +1,5 @@
 ﻿using aoc2020.Puzzles.Core;
-using aoc2020.Puzzles.Extensions;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +9,27 @@ namespace aoc2020.Puzzles.Solutions
     public sealed class Day03 : SolutionBase
     {
         public override async Task<string> Part1Async(string input)
-        { 
-            throw new NotImplementedException();
+        {
+            var terrain = input.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(line => line.Select(c => c is '#').ToArray()).ToArray();
+            var countTrees = CountTrees(terrain, 3, 1);
+
+            return countTrees.ToString();
+        }
+
+        private int CountTrees(bool[][] terrain, int right, int down)
+        {
+            int countTrees = 0;
+            int col = right;
+            int row = down;
+            while (row < terrain.Length)
+            {
+                if (terrain[row][col % terrain[row].Length])
+                    countTrees++;
+                col += right;
+                row += down;
+            }
+
+            return countTrees;
         }
 
         public override async Task<string> Part2Async(string input)
