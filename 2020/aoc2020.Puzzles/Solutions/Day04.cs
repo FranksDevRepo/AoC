292c10d3067b84a9b691b413238af32c3f8f36cb
Frank <frank.speck@txs.de>
2020-12-07 00:40:43 +0100

    solved puzzle day 4 part 1

--- 2020/aoc2020.Puzzles/Solutions/Day04.cs
+++ 2020/aoc2020.Puzzles/Solutions/Day04.cs
@@ -1,5 +1,5 @@
 ﻿using aoc2020.Puzzles.Core;
-using aoc2020.Puzzles.Extensions;
+using MoreLinq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +11,42 @@ namespace aoc2020.Puzzles.Solutions
     public sealed class Day04 : SolutionBase
     {
         public override async Task<string> Part1Async(string input)
-        { 
-            throw new NotImplementedException();
+        {
+            var passports = GetPassports(input);
+            int countValidPassports = 0;
+            foreach (var passport in passports)
+            {
+                countValidPassports += CheckValidPassport(passport) ? 1 : 0;
+            }
+            return countValidPassports.ToString();
         }
 
         public override async Task<string> Part2Async(string input)
         {
             throw new NotImplementedException();
+        }
+
+        private List<Dictionary<string, string>> GetPassports(string input)
+        {
+            var passports = new List<Dictionary<string, string>>();
+            foreach (var passport in input.Split(new string[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                passports.Add(passport.Split(new string[] { "\n", " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToDictionary(x => x.Split(":")[0], x => x.Split(":")[1]));
+            }
+            return passports;
+        }
+
+        private bool CheckValidPassport(Dictionary<string, string> passport)
+        {
+            return
+                passport.ContainsKey("byr")
+                && passport.ContainsKey("iyr")
+                && passport.ContainsKey("eyr")
+                && passport.ContainsKey("hgt")
+                && passport.ContainsKey("hcl")
+                && passport.ContainsKey("ecl")
+                && passport.ContainsKey("pid");
         }
     }
 }
