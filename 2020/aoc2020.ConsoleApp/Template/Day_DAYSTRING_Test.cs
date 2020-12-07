31904c35652fe06d10ba41478aaba672a5f6a0ad
Frank <frank.speck@txs.de>
2020-12-07 16:28:13 +0100

    changed the test-template, so that tests that run against input files will not be forgotten

--- 2020/aoc2020.ConsoleApp/Template/Day_DAYSTRING_Test.cs
+++ 2020/aoc2020.ConsoleApp/Template/Day_DAYSTRING_Test.cs
@@ -1,4 +1,5 @@
 ﻿using aoc2020.Puzzles.Solutions;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -19,5 +20,24 @@ namespace aoc2020.Puzzles.Test.Solutions
             var input = @"";
             Assert.Equal("", await Solution.Part2Async(input));
         }
+
+        [Fact]
+        public async Task Part1WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "dayDAYSTRING.txt"));
+
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "dayDAYSTRING.txt"));
+
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
     }
 }
