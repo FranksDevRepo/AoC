132f4c6c47b5e3bcc61731ee2c710e4a8c2d1d3b
Frank <frank.speck@txs.de>
2020-12-08 00:45:19 +0100

    solved day 7 part 1

--- 2020/aoc2020.Puzzles.Test/Solutions/Day07Test.cs
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day07Test.cs
@@ -1,6 +1,6 @@
-﻿using System.IO;
+﻿using aoc2020.Puzzles.Solutions;
+using System.IO;
 using System.Reflection;
-using aoc2020.Puzzles.Solutions;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -8,10 +8,7 @@ namespace aoc2020.Puzzles.Test.Solutions
 {
     public sealed class Day07Test : TestBase<Day07>
     {
-        [Fact]
-        public async Task Part1()
-        {
-            var input = @"light red bags contain 1 bright white bag, 2 muted yellow bags.
+        private readonly string myInput = @"light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
 bright white bags contain 1 shiny gold bag.
 muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
@@ -20,14 +17,17 @@ dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.";
-            Assert.Equal("4", await Solution.Part1Async(input));
+
+        [Fact]
+        public async Task Part1()
+        {
+            Assert.Equal("4", await Solution.Part1Async(myInput));
         }
 
         [Fact]
         public async Task Part2()
         {
-            var input = @"";
-            Assert.Equal("", await Solution.Part2Async(input));
+            Assert.Equal("", await Solution.Part2Async(myInput));
         }
 
         [Fact]
@@ -36,7 +36,7 @@ dotted black bags contain no other bags.";
             var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var input = File.ReadAllText(Path.Combine(rootDir, "Input", "day07.txt"));
 
-            Assert.Equal("", await Solution.Part1Async(input));
+            Assert.Equal("296", await Solution.Part1Async(input));
         }
 
         [Fact]
--- 2020/aoc2020.Puzzles/Solutions/Day07.cs
+++ 2020/aoc2020.Puzzles/Solutions/Day07.cs
@@ -1,23 +1,73 @@
 ﻿using aoc2020.Puzzles.Core;
-using aoc2020.Puzzles.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace aoc2020.Puzzles.Solutions
 {
+    /*
+     * RegEx patterns
+     * ([faded blue] bags contain) (no other bags).
+     * ^(.*) bags contain no other bags
+     * [light red] bags contain (1 [bright white] bag, 2 [muted yellow] bags).
+     * ^(.*) bags contain \d* (.*) bag, \d* (.*) bags
+     */
+
     [Puzzle("Handy Haversacks")]
     public sealed class Day07 : SolutionBase
     {
         public override async Task<string> Part1Async(string input)
-        { 
-            throw new NotImplementedException();
+        {
+            var lines = GetLines(input);
+            var outerBagRegex = new Regex("^(.*?) bags contain (.*).$", RegexOptions.Compiled);
+            var innerBagRegex = new Regex("\\d* (.*) bag", RegexOptions.Compiled);
+
+            var coloredBagDict = new Dictionary<string, HashSet<string>>();
+
+            foreach (var line in lines)
+            {
+                var outerBagMatch = outerBagRegex.Match(line);
+
+                if (!outerBagMatch.Success)
+                    continue;
+                var outerBagColor = outerBagMatch.Groups[1].Value;
+                var innerBags = outerBagMatch.Groups[2].Value;
+                if (innerBags == "no other bags")
+                    continue;
+
+                foreach (var innerBag in innerBags.Split(',', StringSplitOptions.TrimEntries))
+                {
+                    var innerBagMatch = innerBagRegex.Match(innerBag);
+                    if(!innerBagMatch.Success)
+                        continue;
+
+                    var innerBagColor = innerBagMatch.Groups[1].Value;
+                    if (!coloredBagDict.ContainsKey(innerBagColor))
+                        coloredBagDict.Add(innerBagColor, new HashSet<string>());
+                    coloredBagDict[innerBagColor].Add(outerBagColor);
+                }
+            }
+
+
+            var possibleBagColors = new HashSet<string>();
+            GetPossibleBagColors("shiny gold", coloredBagDict, ref possibleBagColors);
+            return possibleBagColors.Count.ToString();
         }
 
         public override async Task<string> Part2Async(string input)
         {
             throw new NotImplementedException();
+        }
+
+        private void GetPossibleBagColors(string color, Dictionary<string, HashSet<string>> coloredBagDict, ref HashSet<string> possibleBagColors)
+        {
+            foreach (var bagColor in coloredBagDict[color])
+            {
+                possibleBagColors.Add(bagColor);
+                if(coloredBagDict.ContainsKey(bagColor))
+                    GetPossibleBagColors(bagColor, coloredBagDict, ref possibleBagColors);
+            }
         }
     }
 }
