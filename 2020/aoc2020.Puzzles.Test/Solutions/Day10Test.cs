6936d2e2b09185c468327e43d6c413979637bcde
Frank <frank.speck@txs.de>
2020-12-10 06:19:45 +0100

    setup puzzles day 10 part 1 added test for day 10 part 1

---
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day10Test.cs
@@ -0,0 +1,54 @@
+﻿using aoc2020.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2020.Puzzles.Test.Solutions
+{
+    public sealed class Day10Test : TestBase<Day10>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var input = @"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+            Assert.Equal("22", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+        [Fact]
+        public async Task Part1WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day10.txt"));
+
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day10.txt"));
+
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+    }
+}
