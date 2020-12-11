fdd914ef62def52aa092034a8495bc4ed665ad22
Frank <frank.speck@txs.de>
2020-12-11 06:31:07 +0100

    setup puzzles day 11 part 1 added test for day 11 part 1

---
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day11Test.cs
@@ -0,0 +1,54 @@
+﻿using aoc2020.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2020.Puzzles.Test.Solutions
+{
+    public sealed class Day11Test : TestBase<Day11>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var input = @"L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+";
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+        [Fact]
+        public async Task Part1WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day11.txt"));
+
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day11.txt"));
+
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+    }
+}
