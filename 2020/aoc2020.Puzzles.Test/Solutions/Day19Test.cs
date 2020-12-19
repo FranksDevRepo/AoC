0df4892fdbc188d544557bb2b3478991547bf6f7
Frank <frank.speck@txs.de>
2020-12-19 08:42:44 +0100

    day 19 part 1 added test and solution

---
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day19Test.cs
@@ -0,0 +1,55 @@
+﻿using aoc2020.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2020.Puzzles.Test.Solutions
+{
+    public sealed class Day19Test : TestBase<Day19>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var input = @"0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: ""a""
+5: ""b""
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb";
+            Assert.Equal("2", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+        [Fact]
+        public async Task Part1WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day19.txt"));
+
+            Assert.Equal("173", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day19.txt"));
+
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+    }
+}
---
+++ 2020/aoc2020.Puzzles/Solutions/Day19.cs
@@ -0,0 +1,108 @@
+﻿using aoc2020.Puzzles.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aoc2020.Puzzles.Solutions
+{
+    [Puzzle("Monster Messages")]
+    public sealed class Day19 : SolutionBase
+    {
+        public override async Task<string> Part1Async(string input)
+        {
+            var rules = (from line in GetLines(input)
+                where !string.IsNullOrWhiteSpace(line) && line.Contains(':')
+                let rule = line.Split(':', StringSplitOptions.TrimEntries)
+                select rule).ToDictionary(rule => int.Parse(rule[0]), rule => rule[1]);
+
+            var messages = (from line in GetLines(input)
+                where !string.IsNullOrWhiteSpace(line) && !line.Contains(':')
+                select line).ToList();
+
+            var ruleEngine = new RuleEngine(rules);
+            var possibleMessages = ruleEngine.GetRule(0).ToList();
+
+            int numberOfMessages = messages.Intersect(possibleMessages).Count();
+
+            return numberOfMessages.ToString();
+        }
+
+        public override async Task<string> Part2Async(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class RuleEngine
+    {
+        private readonly Dictionary<int, string> rules = new Dictionary<int, string>();
+
+        public RuleEngine(Dictionary<int, string> rules)
+        {
+            this.rules = rules;
+        }
+
+        public IEnumerable<string> GetRule(int index)
+        {
+            var rule = rules[index].Trim('"');
+
+            if (rule.Equals("a") || rule.Equals("b"))
+            {
+                yield return rule;
+                yield break;
+            }
+
+            List<List<int>> subRules = new List<List<int>>();
+            var count = 0;
+
+            var options = rule.Split('|', StringSplitOptions.TrimEntries);
+
+            foreach (var option in options)
+            {
+                subRules.Add(new List<int>());
+                foreach (var entry in option.Split(' ', StringSplitOptions.TrimEntries))
+                {
+                    subRules[count].Add(int.Parse(entry));
+                }
+
+                count++;
+            }
+
+            //if (options.Length == 0)
+            //{
+            //    subRules.Add(new List<int>());
+            //    foreach (var entry in rule.Split(' ', StringSplitOptions.TrimEntries))
+            //    {
+            //        subRules[count].Add(int.Parse(entry));
+            //    }
+
+            //    count++;
+            //}
+
+            foreach (var subRule in subRules)
+            {
+                IEnumerable<string> first = GetRule(subRule[0]);
+                IEnumerable<string> second = subRule.Count > 1 ? GetRule(subRule[1]) : new List<string>();
+                IEnumerable<string> third = subRule.Count > 2 ? GetRule(subRule[2]) : new List<string>();
+                foreach (var f in first)
+                {
+                    foreach (var s in second)
+                    {
+                        foreach (var t in third)
+                        {
+                            yield return $"{f}{s}{t}";
+
+                        }
+
+                        if (!third.Any())
+                            yield return $"{f}{s}";
+                    }
+
+                    if (!second.Any())
+                        yield return $"{f}";
+                }
+            }
+        }
+    }
+}
