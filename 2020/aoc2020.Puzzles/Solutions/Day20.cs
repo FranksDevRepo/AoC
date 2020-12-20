c8d347d2d6d5552140b442e9a4ca98849e370d57
Frank <frank.speck@txs.de>
2020-12-20 19:58:06 +0100

    Day 20 refactor extract methods

--- 2020/aoc2020.Puzzles/Solutions/Day20.cs
+++ 2020/aoc2020.Puzzles/Solutions/Day20.cs
@@ -43,14 +43,14 @@ namespace aoc2020.Puzzles.Solutions
             var lines = (from line in input.Replace("\r", "").Split("\n").ToList()
                 select line).ToArray();
 
-            var nameRegex = new Regex(@"^Tile (?'TileNumber'\d+):$");
+            var tileNumberRegex = new Regex(@"^Tile (?'TileNumber'\d+):$");
             var tiles = new Dictionary<int, List<string>>();
             var tileChecksums = new Dictionary<int, Checksum>();
             var tileSides = new Dictionary<int, List<ushort>>();
-            
-            for (var idx = 0; idx < lines.Length &&!string.IsNullOrWhiteSpace(lines[idx]); ++idx)
+
+            for (var idx = 0; idx < lines.Length && !string.IsNullOrWhiteSpace(lines[idx]); ++idx)
             {
-                var match = nameRegex.Match(lines[idx]);
+                var match = tileNumberRegex.Match(lines[idx]);
 
                 if (!match.Success)
                     throw new InvalidOperationException($"Couldn't find tile number in '{lines[idx]}'");
@@ -64,14 +64,18 @@ namespace aoc2020.Puzzles.Solutions
                 var tileNumber = int.Parse(match.Groups["TileNumber"].Value);
                 tiles.Add(tileNumber, tile);
 
-                checksum.top = Convert.ToUInt16(string.Concat(tile[0].Select((c) => c == '#' ? '1' : '0')),2);
+                checksum.top = Convert.ToUInt16(string.Concat(tile[0].Select((c) => c == '#' ? '1' : '0')), 2);
                 checksum.bottom = Convert.ToUInt16(string.Concat(tile[9].Select((c) => c == '#' ? '1' : '0')), 2);
                 checksum.left = Convert.ToUInt16(string.Concat(tile.Select(c => c[0] == '#' ? '1' : '0')), 2);
                 checksum.right = Convert.ToUInt16(string.Concat(tile.Select(c => c[9] == '#' ? '1' : '0')), 2);
-                checksum.flippedTop = Convert.ToUInt16(string.Concat(tile[0].Select((c) => c == '#' ? '1' : '0').Reverse()), 2);
-                checksum.flippedBottom = Convert.ToUInt16(string.Concat(tile[9].Select((c) => c == '#' ? '1' : '0').Reverse()), 2);
-                checksum.flippedLeft = Convert.ToUInt16(string.Concat(tile.Select(c => c[0] == '#' ? '1' : '0').Reverse()), 2);
-                checksum.flippedRight = Convert.ToUInt16(string.Concat(tile.Select(c => c[9] == '#' ? '1' : '0').Reverse()), 2);
+                checksum.flippedTop =
+                    Convert.ToUInt16(string.Concat(tile[0].Select((c) => c == '#' ? '1' : '0').Reverse()), 2);
+                checksum.flippedBottom =
+                    Convert.ToUInt16(string.Concat(tile[9].Select((c) => c == '#' ? '1' : '0').Reverse()), 2);
+                checksum.flippedLeft =
+                    Convert.ToUInt16(string.Concat(tile.Select(c => c[0] == '#' ? '1' : '0').Reverse()), 2);
+                checksum.flippedRight =
+                    Convert.ToUInt16(string.Concat(tile.Select(c => c[9] == '#' ? '1' : '0').Reverse()), 2);
                 tileChecksums.Add(tileNumber, checksum);
 
                 var sides = new List<ushort>();
@@ -86,51 +90,64 @@ namespace aoc2020.Puzzles.Solutions
                 tileSides.Add(tileNumber, sides);
             }
 
-            var debugOutput = new List<string>();
             if (Debugger.IsAttached)
-            {
-                foreach (var tileChecksum in tileChecksums)
-                {
-                    debugOutput.Add($"Tile  : {tileChecksum.Key}");
-                    debugOutput.Add($"Top   : {tileChecksum.Value.top, 12}");
-                    debugOutput.Add($"Left  : {tileChecksum.Value.left, 12}");
-                    debugOutput.Add($"Right : {tileChecksum.Value.right, 12}");
-                    debugOutput.Add($"Bottom: {tileChecksum.Value.bottom, 12}");
-                    debugOutput.Add(string.Empty);
-                    debugOutput.Add($"Flipped Top   : {tileChecksum.Value.flippedTop,4}");
-                    debugOutput.Add($"Flipped Left  : {tileChecksum.Value.flippedLeft,4}");
-                    debugOutput.Add($"Flipped Right : {tileChecksum.Value.flippedRight,4}");
-                    debugOutput.Add($"Flipped Bottom: {tileChecksum.Value.flippedBottom,4}");
-                    debugOutput.Add("\n");
-                }
-                var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                File.WriteAllLines(
-                    Path.Combine(rootDir, @"..\\..\\..\\..\\aoc2020.Puzzles", "Input", "solution day20.txt"), debugOutput);
+                WriteDebugOutput(tileChecksums);
 
-            }
+            var matchingTiles = FindMatchingTiles(tileSides);
 
-            var matchingTiles = new Dictionary<int, List<int>>();
+            long result = matchingTiles
+                .Where(kvp => kvp.Value.Count == 2)
+                .Select(kvp => (long) kvp.Key)
+                .Aggregate((a, b) => a * b);
 
+            return result.ToString();
+        }
+
+        private static Dictionary<int, List<int>> FindMatchingTiles(Dictionary<int, List<ushort>> tileSides)
+        {
+            Dictionary<int, List<int>> matchingTiles = new Dictionary<int, List<int>>();
             for (int idx1 = 0; idx1 < tileSides.Count; idx1++)
             {
                 var tileMatches = new List<int>();
-                int countMatches = 0;
-                
+
                 for (int idx2 = 0; idx2 < tileSides.Count; idx2++)
                 {
                     if (idx1 == idx2) continue;
-                    countMatches = tileSides.ElementAt(idx1).Value.Intersect(tileSides.ElementAt(idx2).Value).Count();
-                    if(countMatches>0)
+                    bool foundMatchingSides =
+                        tileSides.ElementAt(idx1).Value.Intersect(tileSides.ElementAt(idx2).Value).Any();
+                    if (foundMatchingSides)
                         tileMatches.Add(tileSides.ElementAt(idx2).Key);
                 }
 
-                if (tileMatches.Count > 0)
+                if (tileMatches.Any())
                     matchingTiles.Add(tileSides.ElementAt(idx1).Key, tileMatches);
             }
 
-            long result = matchingTiles.Where(kvp => kvp.Value.Count == 2).Select(kvp => (long)kvp.Key).Aggregate((a, b) => a * b);
+            return matchingTiles;
+        }
 
-            return result.ToString();
+        private static void WriteDebugOutput(Dictionary<int, Checksum> tileChecksums)
+        {
+            var debugOutput = new List<string>();
+            foreach (var tileChecksum in tileChecksums)
+            {
+                debugOutput.Add($"Tile  : {tileChecksum.Key}");
+                debugOutput.Add($"Top   : {tileChecksum.Value.top,12}");
+                debugOutput.Add($"Left  : {tileChecksum.Value.left,12}");
+                debugOutput.Add($"Right : {tileChecksum.Value.right,12}");
+                debugOutput.Add($"Bottom: {tileChecksum.Value.bottom,12}");
+                debugOutput.Add(string.Empty);
+                debugOutput.Add($"Flipped Top   : {tileChecksum.Value.flippedTop,4}");
+                debugOutput.Add($"Flipped Left  : {tileChecksum.Value.flippedLeft,4}");
+                debugOutput.Add($"Flipped Right : {tileChecksum.Value.flippedRight,4}");
+                debugOutput.Add($"Flipped Bottom: {tileChecksum.Value.flippedBottom,4}");
+                debugOutput.Add("\n");
+            }
+
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            File.WriteAllLines(
+                Path.Combine(rootDir, @"..\\..\\..\\..\\aoc2020.Puzzles", "Input", "solution day20.txt"),
+                debugOutput);
         }
 
         public override async Task<string> Part2Async(string input)
