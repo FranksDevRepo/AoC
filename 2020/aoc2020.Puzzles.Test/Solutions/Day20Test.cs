07d812551f8e5a552fce24f31bc20ebc03490c41
Frank <frank.speck@txs.de>
2020-12-20 06:58:27 +0100

    day 20 part 1 added test day 20 part 1

---
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day20Test.cs
@@ -0,0 +1,152 @@
+﻿using aoc2020.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2020.Puzzles.Test.Solutions
+{
+    public sealed class Day20Test : TestBase<Day20>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var input = @"Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###
+
+Tile 1951:
+#.##...##.
+#.####...#
+.....#..##
+#...######
+.##.#....#
+.###.#####
+###.##.##.
+.###....#.
+..#.#..#.#
+#...##.#..
+
+Tile 1171:
+####...##.
+#..##.#..#
+##.#..#.#.
+.###.####.
+..###.####
+.##....##.
+.#...####.
+#.##.####.
+####..#...
+.....##...
+
+Tile 1427:
+###.##.#..
+.#..#.##..
+.#.##.#..#
+#.#.#.##.#
+....#...##
+...##..##.
+...#.#####
+.#.####.#.
+..#..###.#
+..##.#..#.
+
+Tile 1489:
+##.#.#....
+..##...#..
+.##..##...
+..#...#...
+#####...#.
+#..#.#.#.#
+...#.#.#..
+##.#...##.
+..##.##.##
+###.##.#..
+
+Tile 2473:
+#....####.
+#..#.##...
+#.##..#...
+######.#.#
+.#...#.#.#
+.#########
+.###.#..#.
+########.#
+##...##.#.
+..###.#.#.
+
+Tile 2971:
+..#.#....#
+#...###...
+#.#.###...
+##.##..#..
+.#####..##
+.#..####.#
+#..#.#..#.
+..####.###
+..#.#.###.
+...#.#.#.#
+
+Tile 2729:
+...#.#.#.#
+####.#....
+..#.#.....
+....#..#.#
+.##..##.#.
+.#.####...
+####.#.#..
+##.####...
+##..#.##..
+#.##...##.
+
+Tile 3079:
+#.#.#####.
+.#..######
+..#.......
+######....
+####.#..#.
+.#...#.##.
+#.#####.##
+..#.###...
+..#.......
+..#.###...
+";
+            var expected = 1951L * 3079L * 2971L * 1171L;
+            Assert.Equal(expected.ToString(), await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+        [Fact]
+        public async Task Part1WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day20.txt"));
+
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day20.txt"));
+
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+    }
+}
---
+++ 2020/aoc2020.Puzzles/Solutions/Day20.cs
@@ -0,0 +1,23 @@
+﻿using aoc2020.Puzzles.Core;
+using aoc2020.Puzzles.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aoc2020.Puzzles.Solutions
+{
+    [Puzzle("Jurassic Jigsaw")]
+    public sealed class Day20 : SolutionBase
+    {
+        public override async Task<string> Part1Async(string input)
+        { 
+            throw new NotImplementedException();
+        }
+
+        public override async Task<string> Part2Async(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
