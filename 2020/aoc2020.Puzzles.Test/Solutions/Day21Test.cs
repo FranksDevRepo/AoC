bb03649c575f35cb65487eff125fe84feac2fac2
Frank <frank.speck@txs.de>
2020-12-21 11:30:00 +0100

    day 21 added test day 21 part 2

--- 2020/aoc2020.Puzzles.Test/Solutions/Day21Test.cs
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day21Test.cs
@@ -8,21 +8,21 @@ namespace aoc2020.Puzzles.Test.Solutions
 {
     public sealed class Day21Test : TestBase<Day21>
     {
-        [Fact]
-        public async Task Part1()
-        {
-            var input = @"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+        private readonly string myInput = @"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
 trh fvjkl sbzzf mxmxvkd (contains dairy)
 sqjhc fvjkl (contains soy)
 sqjhc mxmxvkd sbzzf (contains fish)";
-            Assert.Equal("5", await Solution.Part1Async(input));
+
+        [Fact]
+        public async Task Part1()
+        {
+            Assert.Equal("5", await Solution.Part1Async(myInput));
         }
 
         [Fact]
         public async Task Part2()
         {
-            var input = @"";
-            Assert.Equal("", await Solution.Part2Async(input));
+            Assert.Equal("mxmxvkd,sqjhc,fvjkl", await Solution.Part2Async(myInput));
         }
 
         [Fact]
