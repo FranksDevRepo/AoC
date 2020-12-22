ed520783da27d1c4875c210a25c5d0a3bf208014
Frank <frank.speck@txs.de>
2020-12-22 08:16:08 +0100

    setup day 22 added test day 22 part 1

---
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day22Test.cs
@@ -0,0 +1,57 @@
+﻿using aoc2020.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2020.Puzzles.Test.Solutions
+{
+    public sealed class Day22Test : TestBase<Day22>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var input = @"Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10
+";
+            Assert.Equal("306", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+        [Fact]
+        public async Task Part1WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day22.txt"));
+
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day22.txt"));
+
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+    }
+}
