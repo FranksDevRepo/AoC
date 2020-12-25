eee406ef322b3dec425b68bb1c77ee8ef5234e4c
Frank <frank.speck@txs.de>
2020-12-25 09:02:03 +0100

    day 25 first test passes

--- 2020/aoc2020.Puzzles/Solutions/Day25.cs
+++ 2020/aoc2020.Puzzles/Solutions/Day25.cs
@@ -11,8 +11,46 @@ namespace aoc2020.Puzzles.Solutions
     public sealed class Day25 : SolutionBase
     {
         public override async Task<string> Part1Async(string input)
-        { 
-            throw new NotImplementedException();
+        {
+            var keys = (from line in GetLines(input)
+                where !string.IsNullOrWhiteSpace(line)
+                select line).ToList();
+
+            var cardsPublicKey = long.Parse(keys.First());
+            var doorsPublicKey = long.Parse(keys.Last());
+
+            var secrectLoopSizeCard = DetermineLoopSize(cardsPublicKey);
+            var secrectLoopSizeDoor = DetermineLoopSize(doorsPublicKey);
+
+            var encryptionKeyCard = TransformSubjectNumber(doorsPublicKey, secrectLoopSizeCard);
+            var encryptionKeyDoor = TransformSubjectNumber(cardsPublicKey, secrectLoopSizeDoor);
+
+            return encryptionKeyDoor.ToString();
+        }
+
+        private long DetermineLoopSize(in long cardsPublicKey)
+        {
+            long result = 0;
+            long loopSize = 0;
+            do
+            {
+                loopSize++;
+                result = TransformSubjectNumber(7, loopSize);
+            } while (result != cardsPublicKey);
+
+            return loopSize;
+        }
+
+        private long TransformSubjectNumber(long subjectNumber, long loopSize)
+        {
+            long result = 1;
+            for (int i = 0; i < loopSize; i++)
+            {
+                result *= subjectNumber;
+                result %= 20201227;
+            }
+
+            return result;
         }
 
         public override async Task<string> Part2Async(string input)
