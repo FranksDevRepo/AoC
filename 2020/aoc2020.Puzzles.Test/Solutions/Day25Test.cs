e5eaa9f815e1e7e2ac8f38638c30d1b5399dd142
Frank <frank.speck@txs.de>
2020-12-25 08:26:08 +0100

    setup day 25 added test part 1

---
+++ 2020/aoc2020.Puzzles.Test/Solutions/Day25Test.cs
@@ -0,0 +1,45 @@
+﻿using aoc2020.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2020.Puzzles.Test.Solutions
+{
+    public sealed class Day25Test : TestBase<Day25>
+    {
+        [Fact]
+        public async Task Part1()
+        {
+            var input = @"5764801
+17807724";
+            Assert.Equal("14897079", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+        [Fact]
+        public async Task Part1WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day25.txt"));
+
+            Assert.Equal("", await Solution.Part1Async(input));
+        }
+
+        [Fact]
+        public async Task Part2WithInputFile()
+        {
+            var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var input = File.ReadAllText(Path.Combine(rootDir, "Input", "Day25.txt"));
+
+            Assert.Equal("", await Solution.Part2Async(input));
+        }
+
+    }
+}
