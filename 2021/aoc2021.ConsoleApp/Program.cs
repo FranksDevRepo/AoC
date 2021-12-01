e51021d9c4ed9b29c1dd38601a6fe4d8677af12c
Frank <f.speck@gmx.de>
2021-12-01 23:24:50 +0100

    🔨 refactor

--- 2021/aoc2021.ConsoleApp/Program.cs
+++ 2021/aoc2021.ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using aoc2021.Puzzles.Core;
+using CommandLine;
+using CommandLine.Text;
+using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,10 +12,6 @@ using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using aoc2021.Puzzles.Core;
-using CommandLine;
-using CommandLine.Text;
-using HtmlAgilityPack;
 
 namespace aoc2021.ConsoleApp;
 
