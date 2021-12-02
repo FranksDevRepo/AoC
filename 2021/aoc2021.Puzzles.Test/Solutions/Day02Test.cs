60e8bfb20df6126f636e36871e02206054f63cc0
Frank <f.speck@gmx.de>
2021-12-02 17:40:00 +0100

    ✔️ solved day 2 part 2

--- 2021/aoc2021.Puzzles.Test/Solutions/Day02Test.cs
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day02Test.cs
@@ -9,23 +9,23 @@ namespace aoc2021.Puzzles.Test.Solutions;
 
 public sealed class Day02Test : TestBase<Day02>
 {
-    [Fact]
-    public async Task Part1()
-    {
-        var input = @"forward 5
+    private readonly string _input = @"forward 5
 down 5
 forward 8
 up 3
 down 8
 forward 2";
-        Assert.Equal("150", await Solution.Part1Async(input));
+
+    [Fact]
+    public async Task Part1()
+    {
+        Assert.Equal("150", await Solution.Part1Async(_input));
     }
 
     [Fact]
     public async Task Part2()
     {
-        var input = @"";
-        Assert.Equal("", await Solution.Part2Async(input));
+        Assert.Equal("900", await Solution.Part2Async(_input));
     }
 
     [Fact]
@@ -43,6 +43,6 @@ forward 2";
         var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day02.txt"));
 
-        Assert.Equal("", await Solution.Part2Async(input));
+        Assert.Equal("1592426537", await Solution.Part2Async(input));
     }
 }
--- 2021/aoc2021.Puzzles/Solutions/Day02.cs
+++ 2021/aoc2021.Puzzles/Solutions/Day02.cs
@@ -10,12 +10,16 @@ public sealed class Day02 : SolutionBase
     public enum Command
     {
         Forward,
-        Backward,
         Up,
         Down
     }
 
-    public struct Position
+    public interface IPosition
+    {
+        void Move(Command command, int units);
+    }
+
+    public struct Position : IPosition
     {
         public int Horizontal { get; set; }
         public int Depth { get; set; }
@@ -31,7 +35,6 @@ public sealed class Day02 : SolutionBase
             this.Horizontal = command switch
             {
                 Command.Forward => this.Horizontal + units,
-                Command.Backward => this.Horizontal - units,
                 _ => this.Horizontal
             };
             this.Depth = command switch
@@ -50,9 +53,15 @@ public sealed class Day02 : SolutionBase
 
     public override string Part1(string input)
     {
+        IPosition position = new Position(0, 0);
+        position = ProcessInputAndMovePosition(input, position);
+        return position.ToString();
+    }
+
+    private static IPosition ProcessInputAndMovePosition(string input, IPosition position)
+    {
         var lines = GetLines(input);
         var instructionParser = new Regex("^(?'command'\\b\\w*?\\b) (?'units'\\d*)", RegexOptions.Compiled);
-        var position = new Position(0, 0);
         foreach (var line in lines)
         {
             var match = instructionParser.Match(line);
@@ -61,13 +70,49 @@ public sealed class Day02 : SolutionBase
             Enum.TryParse(match.Groups["command"].Value, true, out Command command);
             var units = Convert.ToInt32(match.Groups["units"].Value);
             position.Move(command, units);
-
         }
-        return position.ToString();
+
+        return position;
     }
 
     public override string Part2(string input)
     {
-        throw new NotImplementedException();
+        IPosition position = new AimedPosition(0, 0, 0);
+        position = ProcessInputAndMovePosition(input, position);
+        return position.ToString();
+    }
+
+    public struct AimedPosition : IPosition
+    {
+        public int Horizontal { get; set; }
+        public int Depth { get; set; }
+        public int Aim { get; set; }
+
+        public AimedPosition(int horizontal, int depth, int aim)
+        {
+            this.Horizontal = horizontal;
+            this.Depth = depth;
+            this.Aim = aim;
+        }
+
+        public void Move(Command command, int units)
+        {
+            this.Aim = command switch
+            {
+                Command.Down => this.Aim + units,
+                Command.Up => this.Aim - units,
+                _ => this.Aim
+            };
+            (this.Horizontal, this.Depth) = command switch
+            {
+                Command.Forward => (this.Horizontal + units, this.Depth + this.Aim * units),
+                _ => (this.Horizontal, this.Depth)
+            };
+        }
+
+        public override string ToString()
+        {
+            return (this.Horizontal * this.Depth).ToString();
+        }
     }
 }
