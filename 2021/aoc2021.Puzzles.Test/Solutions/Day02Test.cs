62d284bfdffad9f919b1501155f61fb96f12b0ce
Frank <f.speck@gmx.de>
2021-12-02 17:07:42 +0100

    ✔️ solved day1 part1

--- 2021/aoc2021.Puzzles.Test/Solutions/Day02Test.cs
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day02Test.cs
@@ -12,8 +12,13 @@ public sealed class Day02Test : TestBase<Day02>
     [Fact]
     public async Task Part1()
     {
-        var input = @"";
-        Assert.Equal("", await Solution.Part1Async(input));
+        var input = @"forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+        Assert.Equal("150", await Solution.Part1Async(input));
     }
 
     [Fact]
@@ -29,7 +34,7 @@ public sealed class Day02Test : TestBase<Day02>
         var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day02.txt"));
 
-        Assert.Equal("", await Solution.Part1Async(input));
+        Assert.Equal("1524750", await Solution.Part1Async(input));
     }
 
     [Fact]
--- 2021/aoc2021.Puzzles/Solutions/Day02.cs
+++ 2021/aoc2021.Puzzles/Solutions/Day02.cs
@@ -1,17 +1,69 @@
 ﻿using aoc2021.Puzzles.Core;
-using aoc2021.Puzzles.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace aoc2021.Puzzles.Solutions;
 
 [Puzzle("Dive!")]
 public sealed class Day02 : SolutionBase
 {
+    public enum Command
+    {
+        Forward,
+        Backward,
+        Up,
+        Down
+    }
+
+    public struct Position
+    {
+        public int Horizontal { get; set; }
+        public int Depth { get; set; }
+
+        public Position(int horizontal, int depth)
+        {
+            this.Horizontal = horizontal;
+            this.Depth = depth;
+        }
+
+        public void Move(Command command, int units)
+        {
+            this.Horizontal = command switch
+            {
+                Command.Forward => this.Horizontal + units,
+                Command.Backward => this.Horizontal - units,
+                _ => this.Horizontal
+            };
+            this.Depth = command switch
+            {
+                Command.Up => this.Depth - units,
+                Command.Down => this.Depth + units,
+                _ => this.Depth
+            };
+        }
+
+        public override string ToString()
+        {
+            return (this.Horizontal * this.Depth).ToString();
+        }
+    }
+
     public override string Part1(string input)
-    { 
-        throw new NotImplementedException();
+    {
+        var lines = GetLines(input);
+        var instructionParser = new Regex("^(?'command'\\b\\w*?\\b) (?'units'\\d*)", RegexOptions.Compiled);
+        var position = new Position(0, 0);
+        foreach (var line in lines)
+        {
+            var match = instructionParser.Match(line);
+            if (!match.Success)
+                continue;
+            Enum.TryParse(match.Groups["command"].Value, true, out Command command);
+            var units = Convert.ToInt32(match.Groups["units"].Value);
+            position.Move(command, units);
+
+        }
+        return position.ToString();
     }
 
     public override string Part2(string input)
