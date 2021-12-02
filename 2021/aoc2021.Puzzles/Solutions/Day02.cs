4f0621b9bbe9c395795d00dce6e72279264ea6d9
Frank <f.speck@gmx.de>
2021-12-02 17:06:28 +0100

    ✨ setup puzzle for day 2

---
+++ 2021/aoc2021.Puzzles/Solutions/Day02.cs
@@ -0,0 +1,21 @@
+﻿using aoc2021.Puzzles.Core;
+using aoc2021.Puzzles.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2021.Puzzles.Solutions;
+
+[Puzzle("Dive!")]
+public sealed class Day02 : SolutionBase
+{
+    public override string Part1(string input)
+    { 
+        throw new NotImplementedException();
+    }
+
+    public override string Part2(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
