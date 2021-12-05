36e1e521237bae4fd772fb322f895c6cc39bbfaa
Frank <f.speck@gmx.de>
2021-12-05 13:45:20 +0100

    ✔️ solved day 4 part1

--- 2021/aoc2021.Puzzles.Test/Solutions/Day04Test.cs
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day04Test.cs
@@ -2,6 +2,7 @@
 using aoc2021.Puzzles.Solutions;
 using System.IO;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -9,18 +10,37 @@ namespace aoc2021.Puzzles.Test.Solutions;
 
 public sealed class Day04Test : TestBase<Day04>
 {
+    private readonly string _input = @"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+
     [Fact]
     public async Task Part1()
     {
-        var input = @"";
-        Assert.Equal("", await Solution.Part1Async(input));
+        Assert.Equal("4512", await Solution.Part1Async(_input));
     }
 
     [Fact]
     public async Task Part2()
     {
         var input = @"";
-        Assert.Equal("", await Solution.Part2Async(input));
+        Assert.Equal("", await Solution.Part2Async(_input));
     }
 
     [Fact]
@@ -29,7 +49,7 @@ public sealed class Day04Test : TestBase<Day04>
         var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day04.txt"));
 
-        Assert.Equal("", await Solution.Part1Async(input));
+        Assert.Equal("82440", await Solution.Part1Async(input));
     }
 
     [Fact]
--- 2021/aoc2021.Puzzles/Solutions/Day04.cs
+++ 2021/aoc2021.Puzzles/Solutions/Day04.cs
@@ -3,6 +3,11 @@ using aoc2021.Puzzles.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using MoreLinq;
 
 namespace aoc2021.Puzzles.Solutions;
 
@@ -10,12 +15,157 @@ namespace aoc2021.Puzzles.Solutions;
 public sealed class Day04 : SolutionBase
 {
     public override string Part1(string input)
-    { 
-        throw new NotImplementedException();
+    {
+        (string draws, string boards) = GetLines(RemoveLineBreaksAndMultipleSpaces(input)).FirstOrDefault()!.Split(' ', 2, StringSplitOptions.None);
+        var player = new Player(boards);
+        player.CreateBingoBoards();
+        var lastDraw = 0;
+        var unmarkedNumberOnWinningBoard = 0;
+        (lastDraw, unmarkedNumberOnWinningBoard) = player.Play(draws);
+        return (lastDraw * unmarkedNumberOnWinningBoard).ToString();
     }
 
     public override string Part2(string input)
     {
         throw new NotImplementedException();
+    }
+
+    string RemoveLineBreaksAndMultipleSpaces(string s)
+    {
+        s = Regex.Replace(s, @"\s+", " ");
+        return s;
+    }
+
+}
+
+public class Player
+{
+    private readonly int[] boardNumbers;
+    public List<BingoBoard> BingoBoards { get; } = new List<BingoBoard>();
+
+    public Player(string boards)
+    {
+        boardNumbers = boards.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Select(n => Convert.ToInt32(n)).ToArray();
+    }
+
+    public void CreateBingoBoards()
+    {
+        var numbersOfBoardsNeeded = boardNumbers.Length / 25;
+        foreach (var boardNumbersForOneBoard in boardNumbers.Batch(25))
+        {
+            var bingoBoard = new BingoBoard(boardNumbersForOneBoard);
+            BingoBoards.Add(bingoBoard);
+        }
+    }
+
+    public (int lastDraw, int unmarkedNumberOnWinningBoard) Play(string draws)
+    {
+        var drawNumbers = draws.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => Convert.ToInt32(s));
+
+        foreach (var drawNumber in drawNumbers)
+        {
+            foreach (var bingoBoard in BingoBoards)
+            {
+                bingoBoard.Mark(drawNumber);
+                if (bingoBoard.IsWin())
+                {
+                    return (drawNumber, bingoBoard.SumOfUnmarkedNumber());
+                }
+            }
+        }
+
+        return (0, 0);
+    }
+}
+
+public class BingoBoard
+{
+    private (int number, bool isMarked)[,] board;
+    public BingoBoard(IEnumerable<int> boardNumbersForOneBoard)
+    {
+        board = new (int number, bool isMarked)[5, 5];
+        using var enumerator = boardNumbersForOneBoard.GetEnumerator();
+        enumerator.MoveNext();
+        for (int row = 0; row < 5; row++)
+        {
+            for (int col = 0; col < 5; col++)
+            {
+                board[row, col] = (enumerator.Current, false);
+                enumerator.MoveNext();
+            }
+        }
+    }
+
+    public void Mark(int drawNumber)
+    {
+        for (int row = 0; row < 5; row++)
+        {
+            for (int col = 0; col < 5; col++)
+            {
+                if (board[row, col].number == drawNumber)
+                    board[row, col].isMarked = true;
+            }
+        }
+        
+    }
+
+    public bool IsWin()
+    {
+        bool IsWin = false;
+        for (int index = 0; index < 5; index++)
+        {
+            if (IsWinningRow(index) || IsWinningColumn(index))
+            {
+                IsWin=true;
+                break;
+            }
+        }
+        return IsWin;
+    }
+
+    private bool IsWinningColumn(int index)
+    {
+        int countMarkedRows = 0;
+        for (int row = 0; row < 5; row++)
+        {
+            if (board[row, index].isMarked)
+                countMarkedRows++;
+            if (countMarkedRows < row + 1)
+                break;
+        }
+
+        return countMarkedRows == 5;
+    }
+
+    private bool IsWinningRow(int index)
+    {
+        int countMarkedColumns = 0;
+        for (int column = 0; column < 5; column++)
+        {
+            if (board[index, column].isMarked)
+                countMarkedColumns++;
+            if (countMarkedColumns < column + 1)
+                break;
+        }
+
+        return countMarkedColumns == 5;
+    }
+
+    public int SumOfUnmarkedNumber()
+    {
+        int sum = 0;
+        for (int row = 0; row < 5; row++)
+        {
+            for (int column = 0; column < 5; column++)
+            {
+                if (!board[row, column].isMarked)
+                {
+                    sum += board[row, column].number;
+                }
+            }
+        }
+
+        return sum;
     }
 }
