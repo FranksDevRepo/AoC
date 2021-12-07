990d6bc82a5cfe9904d6a96a2a319956cc9e39ec
Frank <f.speck@gmx.de>
2021-12-07 09:00:34 +0100

    ✨ setup puzzle for day 7

---
+++ 2021/aoc2021.Puzzles/Solutions/Day07.cs
@@ -0,0 +1,21 @@
+﻿using aoc2021.Puzzles.Core;
+using aoc2021.Puzzles.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2021.Puzzles.Solutions;
+
+[Puzzle("The Treachery of Whales")]
+public sealed class Day07 : SolutionBase
+{
+    public override string Part1(string input)
+    { 
+        throw new NotImplementedException();
+    }
+
+    public override string Part2(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
