d713c62f44dad20ee9fc5360d027fa4534fdbfad
Frank <f.speck@gmx.de>
2021-12-08 11:55:29 +0100

    ✨ setup puzzle for day 8

---
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day08Test.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using aoc2021.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2021.Puzzles.Test.Solutions;
+
+public sealed class Day08Test : TestBase<Day08>
+{
+    [Fact]
+    public async Task Part1()
+    {
+        var input = @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |
+fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
+fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |
+cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |
+efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |
+gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |
+gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |
+cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |
+ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |
+gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
+fgae cfgab fg bagce";
+        Assert.Equal("26", await Solution.Part1Async(input));
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        var input = @"";
+        Assert.Equal("", await Solution.Part2Async(input));
+    }
+
+    [Fact]
+    public async Task Part1WithInputFile()
+    {
+        var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day08.txt"));
+
+        Assert.Equal("", await Solution.Part1Async(input));
+    }
+
+    [Fact]
+    public async Task Part2WithInputFile()
+    {
+        var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day08.txt"));
+
+        Assert.Equal("", await Solution.Part2Async(input));
+    }
+}
