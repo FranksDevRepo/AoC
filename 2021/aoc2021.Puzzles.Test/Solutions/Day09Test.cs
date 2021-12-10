55c696f081b42f3adacbdb42e8d37a9b7cc2d092
Frank <f.speck@gmx.de>
2021-12-10 23:22:21 +0100

    ✨ setup puzzles for day 9 + 10
    ✅ added test for day 9 part 1
    ✅ added test for day 10 part 1

---
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day09Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using aoc2021.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2021.Puzzles.Test.Solutions;
+
+public sealed class Day09Test : TestBase<Day09>
+{
+    [Fact]
+    public async Task Part1()
+    {
+        var input = @"2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+        Assert.Equal("15", await Solution.Part1Async(input));
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        var input = @"";
+        Assert.Equal("", await Solution.Part2Async(input));
+    }
+
+    [Fact]
+    public async Task Part1WithInputFile()
+    {
+        var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day09.txt"));
+
+        Assert.Equal("", await Solution.Part1Async(input));
+    }
+
+    [Fact]
+    public async Task Part2WithInputFile()
+    {
+        var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day09.txt"));
+
+        Assert.Equal("", await Solution.Part2Async(input));
+    }
+}
