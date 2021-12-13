bf3f0170718c2ba5eb3c2204a6221ba559cbbbb3
Frank <f.speck@gmx.de>
2021-12-13 02:00:37 +0100

    🚧 day 9 and 11 still work to do

--- 2021/aoc2021.Puzzles/Solutions/Day09.cs
+++ 2021/aoc2021.Puzzles/Solutions/Day09.cs
@@ -1,5 +1,6 @@
 ﻿using aoc2021.Puzzles.Core;
 using aoc2021.Puzzles.Extensions;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,49 @@ namespace aoc2021.Puzzles.Solutions;
 public sealed class Day09 : SolutionBase
 {
     public override string Part1(string input)
-    { 
-        throw new NotImplementedException();
+    {
+        var heigtMap = GetHeightMap(input);
+
+        var lowPoints = GetLowPoints(heigtMap);
+
+        return CalculateRiskLevel(lowPoints).ToString();
+    }
+
+    private int[] GetLowPoints(int[][] heigtMap)
+    {
+        List<int> lowPoints = new();
+
+        return lowPoints.ToArray();
+    }
+
+    private int CalculateRiskLevel(int[] lowPoints)
+    {
+        var riskLevel = 0;
+        foreach(var lowPoint in lowPoints)
+        {
+            riskLevel = riskLevel + lowPoint + 1;
+        }
+        return riskLevel;
     }
 
     public override string Part2(string input)
     {
+        //foreach(var subKeyName in Registry.ClassesRoot.GetSubKeyNames())
+        //{
+        //    foreach(var a in Registry.GetValue(subKeyName))
+        //    {
+
+        //    }
+        //}
         throw new NotImplementedException();
+    }
+
+    private int[][] GetHeightMap(string input)
+    {
+        var heightMap = input
+            .Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(row => row
+                .Select(c => Convert.ToInt32(c)).ToArray()).ToArray();
+        return heightMap;
     }
 }
--- 2021/aoc2021.Puzzles/Solutions/Day11.cs
+++ 2021/aoc2021.Puzzles/Solutions/Day11.cs
@@ -10,12 +10,76 @@ namespace aoc2021.Puzzles.Solutions;
 public sealed class Day11 : SolutionBase
 {
     public override string Part1(string input)
-    { 
-        throw new NotImplementedException();
+    {
+        return Part1(input);
+    }
+
+    public string Part1(string input, int numberOfSteps = 100)
+    {
+        var octopusSimulation = new DumpOctopusSimulation(input);
+
+        octopusSimulation.CreateOctopusMap();
+
+        int countFlashes = octopusSimulation.SimulateStepAndCountFlashes();
+
+        return countFlashes.ToString();
     }
 
     public override string Part2(string input)
     {
         throw new NotImplementedException();
     }
+
+    private record Position(ushort X, ushort Y);
+
+    public class DumpOctopusSimulation
+    {
+        private readonly string input;
+        public ushort[][] OctopusMap { get; set; }
+
+        public DumpOctopusSimulation(string input)
+        {
+            this.input = input;
+        }
+
+        public void CreateOctopusMap()
+        {
+            this.OctopusMap = this.input
+                .Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(row => row
+                    .Select(c => Convert.ToUInt16(c)).ToArray()).ToArray();
+        }
+
+        public int SimulateStepAndCountFlashes()
+        {
+            var flashingOctopussies = IncreaseEnergyLevel();
+
+            return flashingOctopussies.Count;
+        }
+
+        private List<(ushort x, ushort y)> IncreaseEnergyLevel()
+        {
+            List<(ushort x, ushort y)> flashingOctopussies = new();
+            for (ushort y = 0; y < this.OctopusMap[0].Length; y++)
+            {
+                for(ushort x = 0; x < this.OctopusMap.Length; x++)
+                {
+                    this.OctopusMap[y][x]++;
+                    if(this.OctopusMap[y][x] == 9)
+                    {
+                        flashingOctopussies.Add((x, y));
+                    }
+                }
+            }
+            return flashingOctopussies;
+        }
+
+        private List<(ushort x, ushort y)> IncreaseAdjacentEnergyLevel((ushort x, ushort y) coordinate)
+        {
+            List<(ushort x, ushort y)> flashingOctopussies = new();
+
+            return flashingOctopussies;
+        }
+    }
+
 }
