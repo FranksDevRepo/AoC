1ec808c249c6a00cfe092e66c8cebc17887e49a0
Frank <f.speck@gmx.de>
2021-12-14 09:27:36 +0100

    ✨ setup puzzle for day 14
    ✅ added test for day 14 part 1

---
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day14Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using aoc2021.Puzzles.Solutions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aoc2021.Puzzles.Test.Solutions;
+
+public sealed class Day14Test : TestBase<Day14>
+{
+    [Fact]
+    public async Task Part1()
+    {
+        var input = @"";
+        Assert.Equal("", await Solution.Part1Async(input));
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        var input = @"";
+        Assert.Equal("", await Solution.Part2Async(input));
+    }
+
+    [Fact]
+    public async Task Part1WithInputFile()
+    {
+        var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day14.txt"));
+
+        Assert.Equal("", await Solution.Part1Async(input));
+    }
+
+    [Fact]
+    public async Task Part2WithInputFile()
+    {
+        var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day14.txt"));
+
+        Assert.Equal("", await Solution.Part2Async(input));
+    }
+}
