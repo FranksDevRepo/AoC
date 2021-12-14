69dda2e8ce5f2e73ef67fad93232207e9fe052e1
Frank <f.speck@gmx.de>
2021-12-14 09:30:21 +0100

    ✔️ solved day 14 part 1
    ✅ added test for day 14 part 2

--- 2021/aoc2021.Puzzles.Test/Solutions/Day14Test.cs
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day14Test.cs
@@ -9,18 +9,35 @@ namespace aoc2021.Puzzles.Test.Solutions;
 
 public sealed class Day14Test : TestBase<Day14>
 {
+    private readonly string _input = @"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+
     [Fact]
     public async Task Part1()
     {
-        var input = @"";
-        Assert.Equal("", await Solution.Part1Async(input));
+        Assert.Equal("1588", await Solution.Part1Async(_input));
     }
 
     [Fact]
     public async Task Part2()
     {
-        var input = @"";
-        Assert.Equal("", await Solution.Part2Async(input));
+        Assert.Equal("2188189693529", await Solution.Part2Async(_input));
     }
 
     [Fact]
@@ -29,7 +46,7 @@ public sealed class Day14Test : TestBase<Day14>
         var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day14.txt"));
 
-        Assert.Equal("", await Solution.Part1Async(input));
+        Assert.Equal("2891", await Solution.Part1Async(input));
     }
 
     [Fact]
--- 2021/aoc2021.Puzzles/Solutions/Day14.cs
+++ 2021/aoc2021.Puzzles/Solutions/Day14.cs
@@ -3,6 +3,8 @@ using aoc2021.Puzzles.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using MoreLinq;
 
 namespace aoc2021.Puzzles.Solutions;
 
@@ -10,8 +12,37 @@ namespace aoc2021.Puzzles.Solutions;
 public sealed class Day14 : SolutionBase
 {
     public override string Part1(string input)
-    { 
-        throw new NotImplementedException();
+    {
+        var polymerTemplate = GetLines(input).First();
+        var rules = GetLines(input)
+            .Where(l => l.Contains("->", StringComparison.InvariantCulture))
+            .Select(l => l.Split("->", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+            .ToDictionary(k => k[0], v => v[1]);
+
+        int steps = 0;
+        StringBuilder polymer = new StringBuilder(polymerTemplate);
+        do
+        {
+            steps++;
+            StringBuilder newPolymer = new();
+            for (int index = 0; index < polymer.Length - 1; index++)
+            {
+                string pair = polymer.ToString()[index..(index + 2)];
+                newPolymer.Append($"{pair[0]}{rules[pair]}");
+            }
+
+            newPolymer.Append(polymer.ToString().Last());
+            polymer.Length = 0;
+            polymer.Append(newPolymer);
+        } while (steps < 10);
+
+        var charCounter = polymer.ToString()
+            .GroupBy(c => c)
+            .Select(c => new { Char = c.Key, Count = c.Count() });
+        var mostCommonElement = charCounter.First(c => c.Count == charCounter.Max(c => c.Count)).Count;
+        var leastCommonElement = charCounter.First(c => c.Count == charCounter.Min(c => c.Count)).Count;
+
+        return (mostCommonElement - leastCommonElement).ToString();
     }
 
     public override string Part2(string input)
