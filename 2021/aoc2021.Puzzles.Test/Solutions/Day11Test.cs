7dcbdda4c37b15f8c298c458d2c7f46ca9acebfe
Frank <f.speck@gmx.de>
2021-12-16 00:29:47 +0100

    ✔️ solved day 11 part 1
    ✅ added test for day 11 part 2

--- 2021/aoc2021.Puzzles.Test/Solutions/Day11Test.cs
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day11Test.cs
@@ -9,10 +9,7 @@ namespace aoc2021.Puzzles.Test.Solutions;
 
 public sealed class Day11Test : TestBase<Day11>
 {
-    [Fact]
-    public async Task Part1()
-    {
-        var input = @"5483143223
+    private readonly string _input = @"5483143223
 2745854711
 5264556173
 6141336146
@@ -22,14 +19,17 @@ public sealed class Day11Test : TestBase<Day11>
 6882881134
 4846848554
 5283751526";
-        Assert.Equal("1656", await Solution.Part1Async(input));
+
+    [Fact]
+    public async Task Part1()
+    {
+        Assert.Equal("1656", await Solution.Part1Async(_input));
     }
 
     [Fact]
     public async Task Part2()
     {
-        var input = @"";
-        Assert.Equal("", await Solution.Part2Async(input));
+        Assert.Equal("195", await Solution.Part2Async(_input));
     }
 
     [Fact]
@@ -38,7 +38,7 @@ public sealed class Day11Test : TestBase<Day11>
         var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         var input = await File.ReadAllTextAsync(Path.Combine(rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day11.txt"));
 
-        Assert.Equal("", await Solution.Part1Async(input));
+        Assert.Equal("1743", await Solution.Part1Async(input));
     }
 
     [Fact]
