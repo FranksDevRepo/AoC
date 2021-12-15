9bf40748aa96ac83c7768d7c2a3f7f7234fe2956
Frank <f.speck@gmx.de>
2021-12-15 15:16:27 +0100

    🚧 day 15 part 2
    - first test with example data passes

--- 2021/aoc2021.Puzzles/Solutions/Day15.cs
+++ 2021/aoc2021.Puzzles/Solutions/Day15.cs
@@ -52,27 +52,84 @@ public sealed class Day15 : SolutionBase
             }
         }
 
-        //Node<int> n1 = graph.AddNode(1);
-        //Node<int> n2 = graph.AddNode(2);
-        //Node<int> n3 = graph.AddNode(3);
-        //Node<int> n4 = graph.AddNode(4);
-        //graph.AddEdge(n1, n2, 9);
-        //graph.AddEdge(n1, n3, 5);
-        //graph.AddEdge(n3, n4, 12);
-        //graph.AddEdge(n2, n4, 18);
-
         var start = nodes[new Coordinate(0, 0)];
         var target = nodes[new Coordinate(map[0].Length - 1, map.Length - 1)];
         var path = graph.GetShortestPathDijkstra(start, target);
-
-        path.ForEach(a => Console.WriteLine(a));
 
         return path.Sum(p => p.Weight).ToString();
     }
 
     public override string Part2(string input)
     {
-        throw new NotImplementedException();
+        var map = input
+            .Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(row => row
+                .Select(c => Convert.ToInt32(Char.GetNumericValue(c))).ToArray()).ToArray();
+
+        map = ResizeMap(map, 5, 5);
+
+        var graph = new Graph<Coordinate>(true, true);
+        Dictionary<Coordinate, Node<Coordinate>> nodes = new();
+
+        for (int y = 0; y < map[0].Length; y++)
+        {
+            for (int x = 0; x < map[0].Length; x++)
+            {
+                var coordinate = new Coordinate(x, y);
+                var node = graph.AddNode(coordinate);
+                nodes.Add(coordinate, node);
+            }
+        }
+
+        for (int y = 0; y < map[0].Length; y++)
+        {
+            for (int x = 0; x < map[0].Length; x++)
+            {
+                var n1 = nodes[new Coordinate(x, y)];
+
+                if (x + 1 < map[0].Length)
+                {
+                    var n2 = nodes[new Coordinate(x + 1, y)];
+                    graph.AddEdge(n1, n2, map[y][x + 1]);
+                    graph.AddEdge(n2, n1, map[y][x]);
+                }
+
+                if (y + 1 < map[0].Length)
+                {
+                    var n3 = nodes[new Coordinate(x, y + 1)];
+                    graph.AddEdge(n1, n3, map[y + 1][x]);
+                    graph.AddEdge(n3, n1, map[y][x]);
+                }
+            }
+        }
+
+        var start = nodes[new Coordinate(0, 0)];
+        var target = nodes[new Coordinate(map[0].Length - 1, map.Length - 1)];
+        var path = graph.GetShortestPathDijkstra(start, target);
+
+        return path.Sum(p => p.Weight).ToString();
+    }
+
+    private int[][] ResizeMap(int[][] map, int resizeX, int resizeY)
+    {
+        var resizedMap = new int[map.Length * resizeY][];
+
+        for (int y = 0; y < map.Length * resizeY; y++)
+        {
+            resizedMap[y] = new int[map[0].Length * resizeX];
+            for (int x = 0; x < map[0].Length * resizeX; x++)
+            {
+                var oldRiskLevel = map[y % map.Length][x % map[0].Length];
+                var newRisklevel = oldRiskLevel + (x / map.Length) + (y / map[0].Length);
+                if (newRisklevel > 9)
+                    newRisklevel = newRisklevel % 10 + 1;
+
+                resizedMap[y][x] = newRisklevel;
+
+            }
+        }
+
+        return resizedMap;
     }
 
     public record Coordinate(int X, int Y);
