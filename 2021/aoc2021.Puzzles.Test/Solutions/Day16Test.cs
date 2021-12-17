946873b314e3f05f92c8a560760afbb60a406edd
Frank <f.speck@gmx.de>
2021-12-17 02:01:24 +0100

    ✔️ solved day 16 part 1 and 2

--- 2021/aoc2021.Puzzles.Test/Solutions/Day16Test.cs
+++ 2021/aoc2021.Puzzles.Test/Solutions/Day16Test.cs
@@ -12,16 +12,16 @@ public sealed class Day16Test : TestBase<Day16>
 {
     [Theory]
     [MemberData(nameof(GetExampleTestcasesPart1))]
-    public async Task Part1(string input, string expected)
+    public async Task Part1(string input, string expectedPart1, string _)
     {
-        Assert.Equal(expected, await Solution.Part1Async(input));
+        Assert.Equal(expectedPart1, await Solution.Part1Async(input));
     }
 
     [Theory]
     [MemberData(nameof(GetExampleTestcasesPart1))]
-    public async Task Part2(string input, string expected)
+    public async Task Part2(string input, string _, string expectedPart2)
     {
-        Assert.Equal(expected, await Solution.Part2Async(input));
+        Assert.Equal(expectedPart2, await Solution.Part2Async(input));
     }
 
     [Fact]
@@ -31,7 +31,7 @@ public sealed class Day16Test : TestBase<Day16>
         var input = await File.ReadAllTextAsync(Path.Combine(
             rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day16.txt"));
 
-        Assert.Equal("", await Solution.Part1Async(input));
+        Assert.Equal("971", await Solution.Part1Async(input));
     }
 
     [Fact]
@@ -41,7 +41,7 @@ public sealed class Day16Test : TestBase<Day16>
         var input = await File.ReadAllTextAsync(Path.Combine(
             rootDir ?? throw new InvalidOperationException("Could not find rootDir."), "Input", "Day16.txt"));
 
-        Assert.Equal("", await Solution.Part2Async(input));
+        Assert.Equal("831996589851", await Solution.Part2Async(input));
     }
 
 
@@ -50,19 +50,19 @@ public sealed class Day16Test : TestBase<Day16>
         {
             new []
             {
-                "8A004A801A8002F478", "16"
+                "8A004A801A8002F478", "16", "15"
             },
             new []
             {
-                "620080001611562C8802118E34", "12"
+                "620080001611562C8802118E34", "12", "46"
             },
             new []
             {
-                "C0015000016115A2E0802F182340", "23"
+                "C0015000016115A2E0802F182340", "23", "46"
             },
             new []
             {
-                "A0016C880162017C3686B18A3D4780", "31"
+                "A0016C880162017C3686B18A3D4780", "31", "54"
             }
         };
 }
